cea6c56ead9f7747f42777402a4271e6a2684ee5
sid8ive <22f3003031@ds.study.iitm.ac.in>
2024-12-26 09:54:11 +0530

    Add files via upload

---
+++ js/credits.js
@@ -0,0 +1,104 @@
+credits={
+    "Foundation": [
+        {
+            "Course": "Mathematics for Data Science I",
+            "Credits": "4"
+        },
+        {
+            "Course": "Statistics for Data Science I",
+            "Credits": "4"
+        },
+        {
+            "Course": "Computational Thinking",
+            "Credits": "4"
+        },
+        {
+            "Course": "English I",
+            "Credits": "4"
+        },
+        {
+            "Course": "Mathematics for Data Science II",
+            "Credits": "4"
+        },
+        {
+            "Course": "Statistics for Data Science II",
+            "Credits": "4"
+        },
+        {
+            "Course": "Programming in Python",
+            "Credits": "4"
+        },
+        {
+            "Course": "English II",
+            "Credits": "4"
+        }
+    ],
+    "Diploma - Programming": [
+        {
+            "Course": "Database Management Systems",
+            "Credits": "4"
+        },
+        {
+            "Course": "Programming, Data Structures and Algorithms using Python",
+            "Credits": "4"
+        },
+        {
+            "Course": "Modern Application Development I",
+            "Credits": "4"
+        },
+        {
+            "Course": "PROJECT Modern Application Development I -\n                            Project",
+            "Credits": "2"
+        },
+        {
+            "Course": "Programming Concepts using Java",
+            "Credits": "4"
+        },
+        {
+            "Course": "Modern Application Development II",
+            "Credits": "4"
+        },
+        {
+            "Course": "PROJECT Modern Application Development II -\n                            Project",
+            "Credits": "2"
+        },
+        {
+            "Course": "System Commands",
+            "Credits": "3"
+        }
+    ],
+    "Diploma - Data Science": [
+        {
+            "Course": "Machine Learning Foundations",
+            "Credits": "4"
+        },
+        {
+            "Course": "Business Data Management",
+            "Credits": "4"
+        },
+        {
+            "Course": "PROJECT Business Data Management - Project",
+            "Credits": "2"
+        },
+        {
+            "Course": "Machine Learning Techniques",
+            "Credits": "4"
+        },
+        {
+            "Course": "Machine Learning Practice",
+            "Credits": "4"
+        },
+        {
+            "Course": "PROJECT Machine Learning Practice - Project",
+            "Credits": "2"
+        },
+        {
+            "Course": "Business Analytics",
+            "Credits": "4"
+        },
+        {
+            "Course": "Tools in Data Science",
+            "Credits": "3"
+        }
+    ]
+}
